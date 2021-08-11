bd4e9d5f85df23ba833ced3c7bed77b00a9df8a9
christophermontero <chris.to20@hotmail.com>
2021-08-11 10:40:14 -0500

    Component Header is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      <h1>App component</h1>
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 
