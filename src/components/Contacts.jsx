e18f88c2ad4476316b58ff91ad73546d93681084
christophermontero <chris.to20@hotmail.com>
2021-08-26 10:50:26 -0500

    Add button to add new contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import ContactsBoard from "./ContactsBoard";
 import FormAddContact from "./FormAddContact";
 import { ContactsReducer } from "./reducers/ContactsReducer.js";
@@ -16,9 +16,17 @@ const Contacts = () => {
     localStorage.setItem("contacts", JSON.stringify(state));
   }, [state]);
 
+  const [formView, setFormView] = useState(false);
+
   return (
     <div className="container mt-3">
-      <FormAddContact dispatch={dispatch} />
+      <button
+        onClick={() => setFormView(!formView)}
+        className="btn btn-success"
+      >
+        {!formView ? "Add contact" : "Close"}
+      </button>
+      {formView && <FormAddContact dispatch={dispatch} />}
       <ContactsBoard contacts={state} dispatch={dispatch} />
     </div>
   );
